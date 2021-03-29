5217761d48e0d33482a9500ec7e4359b90a92d21
yeeun <kye9702@naver.com>
2021-03-29 18:30:51 +0900

    add Location.requestPermissionsAsync()

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
+import { Alert } from 'react-native';
 import React from 'react';
 import Loading from './Loading';
 import * as Location from 'expo-location';
 
 export default class App extends React.Component {
   getLocation = async () => {
-    const location = await Location.getCurrentPositionAsync();
-    console.log(location);
-  }; //Location.getCurrentPositionAsync() 라는 현재 위치를 가져올 수 있는 엑스포 라이브러리를 가져와서 비동기 함수로 만들어 사용 
+    try {
+      await Location.requestPermissionsAsync();
+      const location = await Location.getCurrentPositionAsync();
+      console.log(location);
+    } catch (error) {
+      Alert.alert("Can't find you.", "So sad");
+    }
+  }; 
+  //Location.getCurrentPositionAsync() 라는 현재 위치를 가져올 수 있는 엑스포 라이브러리를 가져와서 비동기 함수로 만들어 사용 
   componentDidMount(){
     this.getLocation();
   }
@@ -17,3 +24,6 @@ export default class App extends React.Component {
     );
   }
 }
+
+//Location.requestPermissionAsync()
+//permission 이 승인됐을 때 resolve된 promise를 리턴한다. 거절 됐을 경우 rejects를 리턴한다.
